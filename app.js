4bb7c0b14dfc497b331abb095a4fd1e4153edf78
johnsonfash <tosin.fashanu@supermartng.com>
2022-02-06 13:37:51 +0100

    changes

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-require('dotenv').config();
-const express = require('express');
-const fileUpload = require('express-fileupload');
+const express = require("express");
 const app = express();
-const server = require('http').Server(app);
-const uuid = require('uuid').v4;
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+app.set("view engine", "ejs");
+const io = require("socket.io")(server, {
+  cors: {
+    origin: '*'
+  }
+});
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
 
-app.set('view engine', 'ejs');
-app.use(express.static('./public'));
-app.use(express.json());
-app.use(fileUpload());
+app.use("/peerjs", peerServer);
+app.use(express.static("public"));
 
-const io = require('socket.io')(server);
-const { ExpressPeerServer } = require('peer');
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidv4()}`);
+});
 
-const peerServer = ExpressPeerServer(server, { debug: true })
-
-app.use('/peerjs', peerServer);
-
-app.get('/', (req, res) => {
-  res.redirect(`/${uuid().substring(0, 7)}`)
-})
-
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room })
-})
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
 
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId, userName) => {
@@ -35,7 +34,4 @@ io.on("connection", (socket) => {
   });
 });
 
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`Server started at port ${port}`);
-})
+server.listen(process.env.PORT || 3030);
